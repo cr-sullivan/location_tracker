515a7d7eb100926d9e56b1a5c93c24b6427f4fbd
Chris Sullivan <cr_sullivan@hotmail.com>
2020-09-04 14:28:09 +1200

    TextField autoFocus and onChanged

--- lib/PositionWidget.dart
+++ lib/PositionWidget.dart
@@ -21,12 +21,13 @@ class PositionState extends State<PositionWidget> {
           Text(position.comment, style: biggerFont),
           TextField(
             controller: _controller..text = position.comment,
+            autofocus: true,
             decoration: new InputDecoration(hintText: "Enter a comment"),
-            // onChanged: (String value) async {
-            //   setState(() {
-            //     position.comment = value;
-            //   });
-            // },  // onChanged
+            onChanged: (String value) async {
+              setState(() {
+                position.comment = value;
+              });
+            },  // onChanged
 
             // onEditingComplete: () {
             //   setState(() {
